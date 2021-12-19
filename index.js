2471246079908df519e5996e5d3a931892579213
DaEagle01 <fbinkarim01@gmail.com>
2021-12-19 11:28:03 +0600

    set up backend

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+// middlewire
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dngm2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("machineLearning");
+    const learnings = database.collection("learnings");
+    const solutionCollecton = database.collection("solutions");
+
+    // get all the soloutions
+    app.get("/solutions", async (req, res) => {
+      const result = await solutionCollecton.find({}).toArray();
+      res.json(result);
+      console.log(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
